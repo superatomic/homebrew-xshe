530ce986decc4a3a0ae7b101c22930d78d6a4ca3
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-04-16 21:35:06 -0500

    Initial Commit

---
+++ Formula/xshe.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Xshe < Formula
+  desc "Set Shell Environment variables across multiple shells with a single configuration file."
+  homepage "https://github.com/superatomic/xshe"
+  license any_of: ["MIT", "Apache-2.0"]
+  head "https://github.com/superatomic/xshe.git"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test xshe`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Xshe
+
+**This repo is still in development. Do not use it.**
+
+## How do I install xshe?
+
+`brew install superatomic/xshe/xshe`
+
+Or `brew tap superatomic/xshe` and then `brew install xshe`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
