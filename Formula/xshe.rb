0ed429cc2e9b7a5dd64a0fde62e7bc07255564ec
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-04-17 16:29:31 -0500

    Finish making xshe.rb
    For https://github.com/superatomic/xshe/issues/39

--- Formula/xshe.rb
+++ Formula/xshe.rb
@@ -1,29 +1,18 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Xshe < Formula
+  version '0.4.0'
   desc "Set Shell Environment variables across multiple shells with a single configuration file."
   homepage "https://github.com/superatomic/xshe"
   license any_of: ["MIT", "Apache-2.0"]
-  head "https://github.com/superatomic/xshe.git", branch: "main"
+  url "https://github.com/superatomic/xshe/archive/v#{version}.zip"
+  sha256 "bae941c2a6bd1e5489dacf8a4c2892f347597e4143b50e1c2781338bf31d8e92"
 
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xshe`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/xshe bash -t 'HELLO = \"World\"'"
   end
 end
